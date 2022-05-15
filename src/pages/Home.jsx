d1b930feed491efbcc9c886996c64dc321ea8f62
Nuno <nunowdev@gmail.com>
2022-05-15 10:43:40 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Hello World!</h1>
+    </div>
+  );
+};
+
+export default Home;
